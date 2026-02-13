b76a793b0551e62f4b0f20bacbccfd5f31a15761
Vanessa <vanessa.omondi@student.moringaschool.com>
2026-02-13 05:19:17 +0300

    fix errors

--- src/__mocks__/api.js
+++ src/__mocks__/api.js
@@ -1,4 +1,15 @@
 // Mock API for testing
+const API_URL = import.meta.env.VITE_API_URL;
+const username = "testuser";
+const password = "test123";
+fetch(`${API_URL}/login`, {
+  method: "POST",
+  headers: {"Content-Type": "application/json"},
+  body: JSON.stringify({username, password})
+})
+  .then(res => res.json())
+  .then(data => console.log(data));
+
 const mockApi = {
   get: jest.fn(() => Promise.resolve({ data: {} })),
   post: jest.fn(() => Promise.resolve({ data: {} })),
